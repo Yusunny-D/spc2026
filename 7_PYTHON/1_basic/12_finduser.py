048720d5089232f29bc10ced2ea6c68deea19a2f
yusun <1223yusun@gmail.com>
2026-05-14 09:42:54 +0900

    파이썬 나머지 공부

--- 7_PYTHON/1_basic/12_finduser.py
+++ 7_PYTHON/1_basic/12_finduser.py
@@ -33,25 +33,44 @@ found_user = find_user_and_retun('구')
 print(found_user)
 
 def find_users2(name:str=None, age:int=None):
-    '''이름 또는 나이를 입력받아 매칭되는 사람을 반환한다.'''
+    # find_user2(나이) 이렇게 넣어서 찾고 싶은데
+    # 파이썬은 2이상의 인자를 받는 함수면 아무 설명없이 인자를 넣으면
+    # 첫번째 인자로 인식 => 그러니까 name=(나이) 이렇게 들어가버림
+    # 그래서 나이만 넣고 싶으면 find_user2(age=나이) 이렇게 써야함
+    # 아니면 로직 안에서 이름과 나이를 따로 구별하는 로직을 만들어야함
+    '''
+        이름 또는 나이를 입력받아 매칭되는 사람을 반환한다.
+        => 이름과 나이, 이름, 나이 이렇게 3가지 방법으로 인자를 받을 수 있음
+    '''
     found_users = []
     for user in users:
-        if name:
-            if user['name'] == name:
+        if name is not None and age is not None:
+            # 이름과 나이를 같이 받았을 때
+            if user['name'] == name and user['age'] == age:
+                # 이름과 나이가 같이 users에 있으면(이름과 나이가 불일치하면 안됨)
                 found_users.append(user)
                 return found_users
-        else:
+        elif name is not None:
+            # 이름을 받았을 때
+            if user['name'] == name:
+                found_user.append(user)
+        elif age is not None:
+            # 나이를 받았을 때
             if user['age'] == age:
                 found_users.append(user)
-                return found_users
+    
+    return found_users
 
-print(find_users2('김민수'))
+# print(find_users2('김민수'))
 
 print('\n', '-'*30)
 
-search_condition = {
-    'name': '김민수'
-}
+def find_users2_better(name=None, age=None, location=None):
+    found_user = []
+    for user in users:
+        # true or 비교문
+        if name is None or user['']
+
 
 def find_user2_best(condition):
     found = []
@@ -65,4 +84,9 @@ def find_user2_best(condition):
 
 
 
+print('\n', '-'*30)
+
+search_condition = {
+    'name': '김민수'
+}
 
