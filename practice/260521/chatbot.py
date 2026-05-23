cce4e823487a17a61ac107ed9b62a9d3f4e335ef
yusun <1223yusun@gmail.com>
2026-05-23 22:46:11 +0900

    GUI완성 및 세션으로 대화 저장

--- practice/260521/chatbot.py
+++ practice/260521/chatbot.py
@@ -11,41 +11,44 @@ client = openai.OpenAI(api_key=openai_api_key)
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
 
+system_role = {'role': 'system', 'content': '당신은 나의 질문에 답변을 주는 챗봇입니다.'}
+
 def session_database(message):
-    chatbot_role = {'role': 'system', 'content': '당신은 나의 질문에 답변을 주는 챗봇입니다.'}
+    chatbot_role = system_role
     if session.get('chat'):
         session['chat'].append(message)
+        session.modified = True
     else:
         session['chat'] = [chatbot_role]
         session['chat'].append(message)
+        session.modified = True
 
 @app.route('/')
 def chatbot():
     return send_from_directory('static', 'index.html')
 
-
 @app.route('/user_input', methods=['POST'])
 def input():
     user_input = request.data.decode('utf-8') # js에서 어떤 데이터 형태로 넘겨주는지 잘 맞춰야함
     user_message = {'role': 'user', 'content': user_input}
-
+    # print(user_input)
     session_database(user_message)
 
     response = client.chat.completions.create(
-        model='gpt-3.5-turbo',
+        model='gpt-4o-mini',
         messages=[
-            {'role': 'system', 'content': '당신은 나의 질문에 답변을 주는 챗봇입니다.'},
+            system_role,
             user_message
         ]
     )
 
     final_res = response.choices[0].message.content
+    # print(final_res)
 
-    return '인풋 받음!'
-
-@app.route('/answer')
-def answer():
-    return '답장 주는 거 구현'
+    bot_message = {'role': 'assistent', 'content': final_res}
+    session_database(bot_message)
+    print(session)
+    return final_res
 
 if __name__ == '__main__':
     app.run(debug=True)
--- practice/260521/static/js/script.js
+++ practice/260521/static/js/script.js
@@ -1,16 +1,28 @@
 const form = document.getElementById('user-input');
+const chat = document.getElementById('chat-view');
 
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', async (e) => {
     e.preventDefault();
+    const user = document.createElement('p')
     const data = new FormData(form);
     
     const inputData = data.get('input')
     console.log(inputData)
+    user.innerText = inputData
+    chat.appendChild(user)
 
-    fetch('/user_input', {
+    const response = await fetch('/user_input', {
         method: 'POST',
         body: inputData
     });
-    
+    const AIreply = await response.text()
+    console.log(AIreply)
+
+    const reply = document.createElement('p')
+    reply.innerText = AIreply
+    chat.appendChild(reply)
+
+    form.querySelector('input').value = '';
+
 });
