7ab68081a99bf737db96ea53c5c54ff6d9153195
yusun <1223yusun@gmail.com>
2026-06-01 09:36:49 +0900

    콜센터 카테고리 분류 chain

--- 10_LangChain/5_tasks/5_callcenter.py
+++ 10_LangChain/5_tasks/5_callcenter.py
@@ -8,10 +8,18 @@ from dotenv import load_dotenv
 
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
 from langchain_core.runnables import RunnableBranch
 
 load_dotenv()
 
 llm = ChatOpenAI(model='gpt-4o-mini')
+
+category_prompt = ChatPromptTemplate.from_template(
+    "고객의 요청사항을 **아래의 3가지 유형으로만** 분류하시오.\n"
+    "(배송조회/결제관련/기술지원)\n\n"
+    "고객 요청사항:\n"
+    "{requirement}"
+)
+category_chain = category_prompt | llm | StrOutputParser()
 
